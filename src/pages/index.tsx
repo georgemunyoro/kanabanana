504a759812ae02ba6089dae1372d54f7bbcabb5b
George Munyoro <george@munyoro.com>
2023-11-20 17:56:55 +0200

    fix: homepage footer and layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,91 +36,109 @@ const styles = tv({
 
 const Home = () => {
   return (
-    <div className="home-gradient h-full overflow-hidden">
+    <div className="home-gradient">
       <Navbar />
-      <ScrollShadow className="h-full">
-        <div className="flex h-full flex-col items-center px-4 py-10">
+      <ScrollShadow className="h-full overflow-x-hidden">
+        <div className="max-w-screen flex h-full flex-col items-center pt-10">
           <div className="flex flex-col items-center justify-center gap-4 pt-10 shadow-black sm:p-10 md:flex-row lg:gap-24">
-            <div className="w-min md:w-fit">
-              <div className="fold:text-center text-left text-4xl font-bold md:text-left md:text-5xl lg:text-6xl">
-                Get stuff done
-                <div>
-                  with kana<span className="text-yellow-500">banana</span>
-                </div>
-              </div>
-              <div className="fold:text-center text-left md:text-left">
-                Organize Effortlessly, Anytime, Anywhere
-              </div>
-              <div className="flex w-full justify-start pt-8">
-                <div className="fold:flex-row fold:justify-center fold:w-full flex flex-col items-start gap-2 md:items-start md:justify-start">
-                  <Link href="/app">
-                    <Button
-                      href="/app"
-                      size="lg"
-                      className="w-fit"
-                      variant="solid"
-                      color="primary"
-                    >
-                      <Rocket size={20} />
-                      Get Started
-                    </Button>
-                  </Link>
-                  <Link href="/app">
-                    <Button
-                      href="/app"
-                      size="lg"
-                      className="w-fit"
-                      variant="flat"
-                      color="success"
-                    >
-                      <User size={20} />
-                      Sign In
-                    </Button>
-                  </Link>
-                </div>
-              </div>
-            </div>
-            <div className="fold:scale-75 h-[500px] scale-[60%] lg:scale-90">
+            <CTA />
+            <div className="h-[500px] scale-[60%] fold:scale-75 lg:scale-90">
               <AnimatedDemo />
             </div>
           </div>
-          <div className="flex grow flex-col pb-48 pt-10 md:w-full md:flex-row lg:max-w-[1000px]">
-            <motion.div className="h-fit w-full grow p-4 md:w-min">
-              <FeatureCard
-                icon={<Move size={20} />}
-                title="Drag-and-Drop Simplicity"
-                description="Easily organize your tasks with a simple drag-and-drop interface. Managing your to-do list has never been more intuitive."
-              />
-            </motion.div>
-            <motion.div className="h-fit w-full grow p-4 md:w-min">
-              <FeatureCard
-                icon={<User size={20} />}
-                title="Your Workspace, Your Way"
-                description="Customize boards and columns to fit the unique flow of your projects. Create a space that truly reflects how you work."
-              />
-            </motion.div>
-            <div className="h-fit w-full grow p-4 md:w-min">
-              <FeatureCard
-                icon={<WifiOff size={20} />}
-                title="Work Offline, Stay Updated"
-                description="Continue managing your tasks even without internet access. Sync up later when you're back online"
-              />
-            </div>
-          </div>
-          <div className="flex w-full items-center justify-center self-end border-t-1 border-gray-800 bg-black p-4 pb-20">
-            <Link href="https://github.com/georgemunyoro/kanabanana">
-              <Button variant="ghost" className="border-0">
-                <Github size={20} />
-              </Button>
-            </Link>
-            <Link href="mailto:george@munyoro.com">
-              <Button variant="ghost" className="border-0">
-                <Envelope size={20} />
-              </Button>
-            </Link>
-          </div>
+          <Features />
+          <Footer />
         </div>
       </ScrollShadow>
+    </div>
+  );
+};
+
+const CTA = () => {
+  return (
+    <div className="w-min md:w-fit">
+      <div className="text-left text-4xl font-bold fold:text-center md:text-left md:text-5xl lg:text-6xl">
+        Get stuff done
+        <div>
+          with kana<span className="text-yellow-500">banana</span>
+        </div>
+      </div>
+      <div className="text-left fold:text-center md:text-left">
+        Organize Effortlessly, Anytime, Anywhere
+      </div>
+      <div className="flex w-full justify-start pt-8">
+        <div className="flex flex-col items-start gap-2 fold:w-full fold:flex-row fold:justify-center md:items-start md:justify-start">
+          <Link href="/app">
+            <Button
+              href="/app"
+              size="lg"
+              className="w-fit"
+              variant="solid"
+              color="primary"
+            >
+              <Rocket size={20} />
+              Get Started
+            </Button>
+          </Link>
+          <Link href="/app">
+            <Button
+              href="/app"
+              size="lg"
+              className="w-fit"
+              variant="flat"
+              color="success"
+            >
+              <User size={20} />
+              Sign In
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Footer = () => {
+  return (
+    <div className="flex w-full items-center justify-center bg-black p-4">
+      <Link href="https://github.com/georgemunyoro/kanabanana">
+        <Button variant="ghost" className="border-0">
+          <Github size={20} />
+        </Button>
+      </Link>
+      <Link href="mailto:george@munyoro.com">
+        <Button variant="ghost" className="border-0">
+          <Envelope size={20} />
+        </Button>
+      </Link>
+    </div>
+  );
+};
+
+const Features = () => {
+  return (
+    <div className="flex grow flex-col px-4 pb-48 pt-10 md:w-full md:flex-row lg:max-w-[1000px]">
+      <div className="h-fit w-full grow p-4 md:w-min">
+        <FeatureCard
+          icon={<Move size={20} />}
+          title="Drag-and-Drop Simplicity"
+          description="Easily organize your tasks with a simple drag-and-drop interface. Managing your to-do list has never been more intuitive."
+        />
+      </div>
+      <div className="h-fit w-full grow p-4 md:w-min">
+        <FeatureCard
+          icon={<User size={20} />}
+          title="Your Workspace, Your Way"
+          description="Customize boards and columns to fit the unique flow of your projects. Create a space that truly reflects how you work."
+        />
+      </div>
+      <div className="h-fit w-full grow p-4 md:w-min">
+        <FeatureCard
+          icon={<WifiOff size={20} />}
+          title="Work Offline, Stay Updated"
+          description="Continue managing your tasks even without internet access. Sync up later when you're back online"
+        />
+      </div>
     </div>
   );
 };
